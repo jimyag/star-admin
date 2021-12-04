908bdbeb9dcfddcb660545caf45e59a551911d15
jimyag <jimyag@126.com>
2021-12-04 10:52:55 +0800

    feat: 初始化项目

---
+++ src/plugin/antui.js
@@ -0,0 +1,6 @@
+// src/main.js
+import Vue from 'vue'
+
+// 使用 ant-design的Button
+import { Button } from 'ant-design-vue';
+Vue.use(Button)
---
+++ src/views/Login.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    登录界面
+    <a-button type="primary">
+      sss
+    </a-button>
+  </div>
+</template>
